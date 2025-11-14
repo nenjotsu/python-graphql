e7ddcaadd046366992b928fdb5568ff7254dabe1
nenjo tsu <26914800+nenjotsu@users.noreply.github.com>
2025-11-14 21:00:12 +0800

    update to UserORM

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-class User(Base):
+class UserORM(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
